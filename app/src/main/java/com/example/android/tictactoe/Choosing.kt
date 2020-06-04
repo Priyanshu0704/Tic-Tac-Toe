f5666baabc66888b0dc7586d50f8b357598edbb6
priyanshu <priyanshubashu7421@gmail.com>
2020-06-04 16:54:42 +0530

    tic tac toe

---
+++ app/src/main/java/com/example/android/tictactoe/Choosing.kt
@@ -0,0 +1,11 @@
+package com.example.android.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Choosing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choosing)
+    }
+}
---
+++ app/src/main/java/com/example/android/tictactoe/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android.tictactoe
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
